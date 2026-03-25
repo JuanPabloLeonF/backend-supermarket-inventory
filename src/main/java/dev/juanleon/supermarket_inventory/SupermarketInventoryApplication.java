f29dd0f43a8516115709d2352e1e454c10a4ef27
Juan Pablo Leon Fonseca <fonseca123leon@gmail.com>
2026-03-25 14:34:27 -0500

    subiendo el esqueleto inicial de mi aplicacion backend

---
+++ src/main/java/dev/juanleon/supermarket_inventory/SupermarketInventoryApplication.java
@@ -0,0 +1,13 @@
+package dev.juanleon.supermarket_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketInventoryApplication.class, args);
+	}
+
+}
